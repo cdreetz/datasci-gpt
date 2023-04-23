94c92d1d4255ecffb81b807ed25bebbfa695cba2
cdreetz <cdreetz@gmail.com>
2023-04-22 19:45:25 -0500

    ipynb to py

---
+++ load.py
@@ -0,0 +1,120 @@
+#project dir: python -m venv .env
+#linux: source .env\bin\activate
+#pip install datasets
+# OR
+#conda install -c huggingface -c conda-forge datasets
+from datasets import load_dataset, DatasetDict
+from transformers import AutoTokenizer
+from transformers import AutoTokenizer, GPT2LMHeadModel, AutoConfig
+from transformers import DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+
+
+# load dataset
+#from datasets import load_dataset, DatasetDict
+
+ds_train = load_dataset("huggingface-course/codeparrot-ds-train", split="train")
+ds_valid = load_dataset("huggingface-course/codeparrot-ds-valid", split="validation")
+
+raw_datasets = DatasetDict(
+    {
+        "train": ds_train,  # .shuffle().select(range(50000)),
+        "valid": ds_valid,  # .shuffle().select(range(500))
+    }
+)
+
+# tokenize dataset
+#from transformers import AutoTokenizer
+
+context_length = 128
+tokenizer = AutoTokenizer.from_pretrained("/huggingface-course/code-search-net-tokenizer")
+
+outputs = tokenzier(
+    raw_datasets["train"][:2]["content"],
+    truncation=True,
+    max_length=context_length,
+    return_overflowing_tokens=True,
+    return_length=True,
+)
+
+def tokenize(element):
+    outputs = tokenzier(
+        element["content"],
+        truncation=True,
+        max_length=context_length,
+        return_verflowwing_tokens=True,
+        return_length=True,
+    )
+    input_batch = []
+    for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
+        if length == context_length:
+            input_batch.append(input_ids)
+    return {"input_ids": input_batch}
+
+
+tokenized_datasets = raw_datasets.map(
+    tokenize, batched=True, remove_columns=raw_datasets["train"].column_names
+)
+
+# initialize new model
+#from transformers import AutoTokenizer, GPT2LMHeadModel, AutoConfig
+
+config = AutoConfig.from_pretrained(
+    "gpt2",
+    vocab_size=len(tokenizer),
+    n_ctx=context_length,
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+)
+
+model = GPTLMHeadModel(config)
+model_size = sum(t.numel() for t in model.parameters())
+#print(f"GPT-2 size: {model_size/1000**2:.1f}M parameters")
+
+# create batches with data collator
+#from transformers import DataCollatorForLanguageModeling
+
+tokenizer.pad_token = tokenizer.eos_token
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+
+# connect to huggingface hub to push the model and tokenizer
+#from huggingface_hub import notebook_login
+
+#notebook_login()
+
+# configure training arguements
+#from transformers import Trainer, TrainingArguments
+
+args = TrainingArguments(
+    output_dir="codeparrot-ds",
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    evaluation_strategy="steps",
+    eval_step=5_000,
+    logging_steps=5_000,
+    gradient_accumulation_steps=8,
+    num_train_epochs=1,
+    weight_decay=0.1,
+    warmup_steps=1_000,
+    lr_scheduler_type="cosine",
+    learning_rate=5e-4,
+    save_steps=5_000,
+    fp16=True,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=args,
+    data_collator=data_collator,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["valid"]
+)
+
+# start trainer
+trainer.train()
+
+# after training completes push to Hub
+#trainer.push_to_hub()
